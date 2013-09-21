a7b16b825b92f3a7942c0fba9fa96076e79dc908
Jesús Espino <jesus.espino@kaleidos.net>
2013-09-21 23:53:24 +0200

    Adding grappelli

--- src/settings.py
+++ src/settings.py
@@ -121,10 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
+    'grappelli',
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
     'disqus',
     'rest_framework',
     'sampledatahelper',
